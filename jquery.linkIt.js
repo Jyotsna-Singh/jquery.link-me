4fd55798b1f411ee0de6ad27881543e89e846a1d
Jyotsna-Singh <jyotsna_singh94@yahoo.co.in>
2016-12-24 18:39:59 +0530

    0.2.0 release

---
+++ jquery.linkIt.js
@@ -0,0 +1,37 @@
+/*
+ * name: LinkIt
+ * author: Jyotsna Singh
+ * version: 0.1.0
+ *license: MIT
+ */
+
+(function($){
+	
+	$.fn.linkIt = function(options){
+		//Default Settings
+		var settings = $.extend({
+			href: null,
+			text: null,
+			target: '_self'
+		}, options);
+		
+		//Validation
+		if(settings.href == null){
+			console.log('You Need an href option for LinkIt to Work');
+			return this;
+		}
+		
+		return this.each(function(){
+			var object = $(this);
+			
+			if(settings.text == null){
+				settings.text = object.text();
+			}
+			object.wrap('<a target="'+settings.target+'" href="'+settings.href+'"></a>').text(settings.text);
+		});
+		
+		
+		
+	}
+	
+} (jQuery));
